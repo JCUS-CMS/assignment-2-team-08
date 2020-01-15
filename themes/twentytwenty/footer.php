c35c3719554246f207bb9f132f4eda037ee84576
Thawe <homnanthawehtun@my.jcu.edu.au>
2020-01-15 12:54:24 +0800

    Initial Commit

--- themes/twentytwenty/footer.php
+++ themes/twentytwenty/footer.php
@@ -30,7 +30,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by Strong and Powerful Thwae Htun', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
