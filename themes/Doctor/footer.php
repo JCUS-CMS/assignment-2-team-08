5d8e3e6d4712d1c1dc518f56de9ffff1e5c0fdde
K Phoo Thaw Naing <kayphoothawnaing@Ks-MacBook-Pro.local>
2020-01-25 03:08:51 +0800

    footer

--- themes/Doctor/footer.php
+++ themes/Doctor/footer.php
@@ -1,17 +1,15 @@
 <footer>
 
-	<<!-- ?php
-		wp_nav_menu(
-			array(
-
-				'theme_location' =>'footer-menu','menu_class' => 'FooterMenu'
-			)
-		);
-    ?> -->
+<div id= "ttr_footer">
+	 Copyright@CMSTeam8
+</div>
 </footer>
 
-	<?php wp_footer();?>
-</body	>
+
+		
+		    	
+
+</body>
 <html>
 
 
